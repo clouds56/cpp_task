42267c43150ed92a2ca09f5a224d2b6b2fb1dda9
Clouds Flowing <clouds.flowing@gmail.com>
2017-05-04 23:36:23 +0800

    init repo

---
+++ library.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Clouds Flowing on 5/4/17.
+//
+
+#include "library.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ library.h
@@ -0,0 +1,10 @@
+//
+// Created by Clouds Flowing on 5/4/17.
+//
+
+#ifndef CPP_TASK_LIBRARY_H
+#define CPP_TASK_LIBRARY_H
+
+void hello();
+
+#endif //CPP_TASK_LIBRARY_H
